f3ad37a3d94222d4383348d783f58bfb3c3319e8
ANBU <anbubalu004@gmail.com>
2025-09-18 15:23:26 +0530

    Add files via upload

--- Assignment 6/divisible_by_5tb.v
+++ Assignment 6/divisible_by_5tb.v
@@ -1,27 +1,35 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module divisible_by_5tb;
-    reg clk, reset, din;
-    wire dout;
-
-    divisible_by_5 uut (.clk(clk), .reset(reset), .din(din), .dout(dout));
-    initial clk = 0;
-    always #5 clk = ~clk;
-     initial begin
-      $monitor ("time=%0t | din=%d | dout=%d", $time, din, dout);
-      reset = 1; din = 0;
-      #10;
-      reset = 0;
-      
-      din = 0; #10;
-      din = 1; #10;
-      din = 2; #10;
-      din = 3; #10;
-      din = 4; #10;
-      #20;
-      $finish;
-     end
-   endmodule 
-      
-      
+module divisible_by_5tb();
+    reg clk;
+    reg reset;
+    reg din;
+    wire divisible;
     
+divisible_by_5 uut (.clk(clk), .reset(reset), .din(din), .divisible(divisible));
+ initial clk = 0;
+ always #5 clk = ~clk;
+ 
+ initial begin
+    $display ("time din divisible comment");
+    $monitor ("time=%0t din=%d divisible=%d", $time, din, divisible);
+    reset=1; din=0;
+    #12;
+    reset=0;
+
+        din = 1; #10;  
+        din = 0; #10;   
+        din = 1; #10;  
+        din = 1; #10;
+        din = 1; #10;
+        din = 1; #10;
+        din = 1; #10;
+        din = 0; #10;
+        din = 1; #10;
+        din = 0; #10;
+        din = 0; #10;
+
+        #20;
+        $finish;
+    end
+endmodule
